9b4946be8a1cfed0df8f2ad70162f8a8d60024aa
Nevin <nevinajithkumar@gmail.com>
2025-02-10 16:34:31 +0530

    training updated on ubuntu

--- train_img.sh
+++ train_img.sh
@@ -1,5 +1,9 @@
+echo 'Hellooo'
+sleep 1h
+echo 'Rerun'
+
 python rerunTraining.py \
-    --resume_from outputs/030-MMDiT-B-2/checkpoint_0571000.pt \
+    --resume_from outputs/030-MMDiT-B-2/checkpoint_0720000.pt \
     --model MMDiT-B/2 \
     --image_size 256 \
     --batch_size 32 \
