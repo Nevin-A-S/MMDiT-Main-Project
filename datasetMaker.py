ed4907752aaee5878014a8aacbeaed645440ad2d
Nevin <nevinajithkumar@gmail.com>
2025-03-09 17:52:48 +0000

    dataset_Inference_maker

--- datasetMaker.py
+++ datasetMaker.py
@@ -1,21 +1,20 @@
 import os
 import pandas as pd
 
-base_dir = "dataPreprocessing"
+base_dir = "test_folder"
 captions = []
-image = []
+images = []
 
-for files in os.listdir(base_dir):
-    if files.endswith('.txt'):
-        with open(os.path.join(base_dir,files)) as f:
-            s = f.read()
-            captions.append(s)
-        file = files[:-4]
-        file = file [:-7]
-        file = file + '.png'
-        print(file)
-        image.append((os.path.join(base_dir,files)))
+for file in os.listdir(base_dir):
+    if file.endswith('.txt'):
+        with open(os.path.join(base_dir, file), "r") as f:
+            caption = f.read().strip()
+            captions.append(caption)
 
+        image_filename = file.replace("_prompt.txt", ".png")  
+        images.append(os.path.join(base_dir, image_filename))
 
-dataframe  = pd.DataFrame(image,captions)
+dataframe = pd.DataFrame({'Image': images, 'Caption': captions})
+
+print(dataframe)
 dataframe.to_csv("synthetic_dataset.csv", index=False)
